475dab80b4a2cb17e7308730f30a204cc6874926
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-27 12:15:18 -0800

    Initial Commit

---
+++ camera_capture_in_class/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  camera_capture_in_class
+//
+//  Created by Mialsy on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
