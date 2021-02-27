626ed857f9a273a5a266661fd3db1bc38262d15c
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-27 13:22:52 -0800

    add code

--- camera_capture_in_class/ViewController.swift
+++ camera_capture_in_class/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let pickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerController.sourceType = UIImagePickerController.SourceType.camera
+        pickerController.delegate = self
+        
     }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        present(pickerController, animated: true, completion: nil)
+    }
+}
 
+extension ViewController : UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("The camera has been closed")
+    }
+}
 
+extension ViewController : UINavigationControllerDelegate {
+    
 }
 
